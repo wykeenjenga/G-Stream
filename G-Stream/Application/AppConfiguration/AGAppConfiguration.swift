5cdbe82af26a7aae5304fca1c1e7c397d36c1e1f
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-07-28 13:17:26 +0300

    Updates

--- G-Stream/Application/AppConfiguration/AGAppConfiguration.swift
+++ G-Stream/Application/AppConfiguration/AGAppConfiguration.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct APAppConfigurations {
+    
+    static var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+    
+    static var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+}
