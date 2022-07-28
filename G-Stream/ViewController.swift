b41790804773f947683920fc7aac864964d3285a
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-07-28 12:21:56 +0300

    Initial Commit

---
+++ G-Stream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G-Stream
+//
+//  Created by Wykee Njenga on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ G-StreamUITests/G_StreamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  G_StreamUITestsLaunchTests.swift
+//  G-StreamUITests
+//
+//  Created by Wykee Njenga on 7/28/22.
+//
+
+import XCTest
+
+class G_StreamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
