415469808b7b03a9da88ec4565dffc465cdccb9d
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-08-03 16:20:52 +0300

    Updates

--- G-Stream/PresentationModules/ScheduleEventsModule/ViewModel/AGScheduledViewModel.swift
+++ G-Stream/PresentationModules/ScheduleEventsModule/ViewModel/AGScheduledViewModel.swift
@@ -2,7 +2,7 @@
 //  AGScheduledViewModel.swift
 //  G-Stream
 //
-//  Created by Wykee Njenga on 7/30/22.
+//  Created by Wykee Njenga on 8/3/22.
 //  Copyright © 2022 G-Stream. All rights reserved.
 //
 
