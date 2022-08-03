76f898b9e2adaa2aa997b1f034a275745c852aba
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-08-03 16:15:48 +0300

    Updates

--- G-Stream/PresentationModules/ScheduleEventsModule/ViewController /AGScheduleViewController.swift
+++ G-Stream/PresentationModules/ScheduleEventsModule/ViewController /AGScheduleViewController.swift
@@ -2,7 +2,7 @@
 //  AGScheduleViewController.swift
 //  G-Stream
 //
-//  Created by Wykee Njenga on 8/3/22.
+//  Created by Wykee Njenga on 8/03/22.
 //  Copyright © 2022 G-Stream. All rights reserved.
 //
 
